fbfe95be6231140f6cdb7f0f299209d040485b6a
Caio Regino <caioregino.147@gmail.com>
2022-06-24 17:56:47 -0300

    Finaliza exercicio 5

--- fundamentos/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-02/scripts.js
+++ fundamentos/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-02/scripts.js
@@ -15,9 +15,11 @@
 // console.log(menu);
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maiorValor = 0;
 for(let index = 0; index < numbers.length; index += 1 ){
-    soma += numbers[index];
+    if(numbers[index] > maiorValor){
+        maiorValor = numbers[index];
+    }
 }
 
-console.log(soma / numbers.length);
+console.log(maiorValor);
