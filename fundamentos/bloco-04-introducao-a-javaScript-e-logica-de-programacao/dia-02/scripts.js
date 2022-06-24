f322d77d6dd93ec678c950fbf2a92170e34ba041
Caio Regino <caioregino.147@gmail.com>
2022-06-24 17:47:42 -0300

    Finaliza exercicio 1

--- fundamentos/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-02/scripts.js
+++ fundamentos/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-02/scripts.js
@@ -8,8 +8,14 @@
 
 // console.log(indexOfPortfolio);
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
-menu.push("Contato");
+// menu.push("Contato");
 
-console.log(menu);
+// console.log(menu);
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let index = 0; index < numbers.length; index += 1 ){
+    console.log(numbers[index]);
+}
